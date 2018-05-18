b74ce92434f1c0435798f9fdd436ccba808cf0c7
Michael Hoffman <michael.hoffman@ge.com>
2018-05-18 14:06:06 -0400

    update

--- attributes/default.rb
+++ attributes/default.rb
@@ -43,6 +43,17 @@ default['kernel_modules']['packages'] = value_for_platform_family(
       'kmod',
     ],
   ),
+  amazon: value_for_platform(
+    %w[amazon] => {
+      '~> 2' => [
+        'systemd'
+        'kmod'
+      ]
+    },
+    'default' => [
+      'kmod',
+    ],
+  )
 )
 
 # Modules init loading requirements
